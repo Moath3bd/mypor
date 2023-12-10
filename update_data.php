a9c46d62e8e327df20625b71c9b625eac31a2d3d
moath3bd <moath3bd@gmail.com>
2023-12-10 14:32:32 +0300

    update

---
+++ update_data.php
@@ -0,0 +1,16 @@
+<?php
+include 'config.php';
+
+$user_id = 1;  // Replace with the actual user ID
+$new_username = "new_username";
+
+$sql = "UPDATE users SET username='$new_username' WHERE id=$user_id";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . $conn->error;
+}
+
+$conn->close();
+?>
